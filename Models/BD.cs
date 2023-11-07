feb42dc379114af72362ce746a8146da85556d18
ILÁN AVERBUCH <47756465@ort>
2023-11-07 08:49:46 -0300

    Empece el model, es 1 funcion por cada store procedure

---
+++ Models/BD.cs
@@ -0,0 +1,15 @@
+using Dapper;
+using System.Data.SqlClient;
+
+public static class BD {
+    public static string _connectionString = @"Server=localhost;DataBase=FuthubBD;Trusted_Connection=True;";
+    
+    public static List<Jugador> GetJugadorByID(int idjug){
+        LisT<Jugador> ListaJugador = null;
+        using(SqlConnection db = new SqlConnection(_connectionString)){
+            string sp = "GetJugadorByID";
+            ListaJugador = db.Query<Jugador>(sp, new { idJugador = idjug}, commandType: CommandType.StoredProcedure).ToLiST();
+        }
+        return ListaJugador;
+    }
+}
