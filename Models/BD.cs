cac026447b0b0e81c50dc5821170b1d8df4b740d
RAMIRO SUED <47958668@ort>
2023-11-09 13:44:00 -0300

    .

--- Models/BD.cs
+++ Models/BD.cs
@@ -5,7 +5,7 @@ public static class BD {
     public static string _connectionString = @"Server=localhost;DataBase=FuthubBD;Trusted_Connection=True;";
     
     public static List<Jugador> GetJugadorByID(int idjug){
-        LisT<Jugador> ListaJugador = null;
+        List<Jugador> ListaJugador = null;
         using(SqlConnection db = new SqlConnection(_connectionString)){
             string sp = "GetJugadorByID";
             ListaJugador = db.Query<Jugador>(sp, new { idJugador = idjug}, commandType: CommandType.StoredProcedure).ToLiST();
