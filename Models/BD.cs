6bcbcf2074758a5aaf8bad8e26532d117b32a6fe
ILÁN AVERBUCH <47756465@ort>
2023-11-29 13:54:14 -0300

    Lo TERMINEEEEE

--- Models/BD.cs
+++ Models/BD.cs
@@ -4,15 +4,15 @@ using System.Data;
 namespace TP11.Models;
 
 public static class BD {
-    public static string _connectionString = @"Server=DESKTOP-K5G1LDU\SQLEXPRESS;DataBase=FuthubBD;Trusted_Connection=True;";
+    public static string _connectionString = @"Server=localhost;DataBase=FuthubBD;Trusted_Connection=True;";
     
    public static void CrearUsuario(Usuario us)
     {
         using (SqlConnection BD = new SqlConnection(_connectionString))
         {
-            string sql = "INSERT INTO Usuario([username],[contraseña],[mail]) VALUES(@Username,@Contraseña,@mail)";
+            string sql = "INSERT INTO Usuario([username],[contraseña],[email]) VALUES(@Username,@Contraseña,@mail)";
             BD.Execute(sql, new { username = us.username, contraseña = us.contraseña, mail = us.mail});
-        }.
+        }
     }
 
     public static void CambiarContraseña(string Username, string nuevaContraseña)
@@ -37,7 +37,7 @@ public static class BD {
 
     public static string GetContraseñaPorUsername(string username)
     {
-        string contraseña;
+        string contraseña = null;
         using (SqlConnection BD = new SqlConnection(_connectionString))
         {
             string sql = "SELECT contraseña FROM Usuario WHERE username=@Username";
@@ -76,7 +76,7 @@ public static class BD {
         List<Jugador> ListaJugadores = null;
         using(SqlConnection db = new SqlConnection(_connectionString)){
             string sp = "GetJugadoresByEquipo";
-            ListaJugadores = db.Query<Jugador>(sp, new { IdEquipoActual = idteam}, commandType: CommandType.StoredProcedure).ToList();
+            ListaJugadores = db.Query<Jugador>(sp, new { IdEquipo = idteam}, commandType: CommandType.StoredProcedure).ToList();
         }
         return ListaJugadores;
     }
@@ -133,7 +133,7 @@ public static class BD {
         using(SqlConnection db = new SqlConnection(_connectionString)){
             string sp = "InsertarJugador";
             db.Execute(sp, new {IdPais = jug.IdPais,
-            IdEquipoActual = jug.IdEquipoActual,
+            IdEquipo = jug.IdEquipo,
             Nombre = jug.Nombre,
             Imagen = jug.ImagenJugador,
             Edad = jug.Edad,
